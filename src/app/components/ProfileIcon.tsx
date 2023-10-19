4cf4bda656764a4819863c5327b0095a4344f528
sayan pramanick <sayanp05@gmail.com>
2023-10-19 21:56:44 +0530

    removed unused code

--- src/app/components/ProfileIcon.tsx
+++ src/app/components/ProfileIcon.tsx
@@ -1,13 +1,13 @@
 // pages/index.js (or any other file where you want to use the Avatar)
-import Avatar from "../../../common/avatar";
-import avataricon from "../../../styles/Avatar.module.css";
+import Image from "next/image";
 const ProfileIcon = () => {
   return (
     <div>
-      <Avatar
-        imageUrl="/DP-Tech Hunter Bhai.png"
-        alt="Your Name"
-        className={avataricon}
+      <Image
+        src="/DP-Tech Hunter Bhai.png"
+        alt="Tech Hunter"
+        width={10}
+        height={10}
       />
     </div>
   );
